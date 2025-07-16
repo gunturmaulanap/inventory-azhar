06b468c40365e66dc2e3c766a395d3666abfadc1
gunturmaulanap <gunturmaulanaputraprojo@gmail.com>
2025-07-16 16:26:13 +0700

    vite tambah

---
+++ config/vite.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'manifest_path' => public_path('build/manifest.json'),
+
+    'hot_file' => public_path('hot'),
+
+    'dev_server' => [
+        'url' => 'http://localhost:5173',
+    ],
+
+    'build_directory' => 'build',
+
+    'asset_url' => env('ASSET_URL', null),
+
+];
